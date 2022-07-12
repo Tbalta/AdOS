d8e95b623d31d548283de91e358704e9dbd6cddc
Tanguy Baltazart <tanguy.baltazart@epita.fr>
2022-07-12 21:32:00 +0200

    more thing inside the runtime.

--- main.adb
+++ main.adb
@@ -27,6 +27,11 @@ begin
 
    x86.gdt.initialize_gdt;
    SERIAL.send_string ("finish");
+   declare
+      test : Interfaces.Unsigned_32 := Interfaces.Unsigned_32 (25);
+   begin
+      SERIAL.send_string (Interfaces.Unsigned_32'Image (test));
+   end;
    --  Loop forever.
    while True loop
       null;
